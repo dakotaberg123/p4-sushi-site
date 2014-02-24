82a5e47d603b2c46954fc6f2aa72b195a84b417d
Berg <dberg@engr-134-04.eastern.ewu.edu>
2014-02-23 17:23:20 -0800

    Initial/Final Commit
    Committed all after building locally without Git.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	// Use both "animated" & "bounceOutLeft" class
+	$('.galleryimg').mouseenter( function(){
+
+		$(this).addClass('animated slideInLeft');
+
+	});
+
+
+
+
+	console.log('Page Loaded. Lets Do this!');
+
+}); 
+
+
+
